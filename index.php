d7388773f3906a78b00047a4e20b7f99b50d2049
Schön Ferenc <65657182+SchonFerenc@users.noreply.github.com>
2020-05-20 14:37:06 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="hu">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@1,200&display=swap" rel="stylesheet">
+    <title>ColorClock</title>
+</head>
+
+<body>
+
+    <div id="clock"></div>
+
+</body>
+
+<script src="js.js"></script>
+
+</html>
